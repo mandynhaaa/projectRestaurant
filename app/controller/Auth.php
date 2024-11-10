c2910519327dd3f9981d53ef70fe43e6ee530249
mandynhaaa <amandatobler18@gmail.com>
2024-11-09 23:23:18 -0300

    feat: adjust authentication with routes

--- app/controller/Auth.php
+++ app/controller/Auth.php
@@ -2,33 +2,76 @@
 
 namespace App\Controller;
 
-require __DIR__ . '/../../vendor/autoload.php';
+use App\Service\AuthService;
 
-use App\Controller\Main;
-use App\Model\User;
+class Auth {
 
-class Auth  {
+    protected $container;
+    private $authService;
 
-    public function isLoggedIn()
-    {
-        session_start();
-        return !empty($_SESSION['user']);
+    public function __construct($container) {
+        $this->container = $container;
+        $this->authService = new AuthService();
     }
 
-    public function login()
-    {
-        $userName = Main::validateParameter($_POST['userName']);
-        $password = Main::validateParameter($_POST['password']);
-        $user = new User($userName, $password, 0);
-        $user->insert();
+    public function viewCreateAccount() {
+        $this->render('createAccount');
     }
 
-    public function logout()
-    {
-        session_start();
-        session_destroy();
+    public function viewLogin() {
+        $this->authService->logout();
+        $this->render('login');
     }
-    
+
+    public function viewHome() {
+        $this->render('home');
+    }
+
+    public function login(array $data): void
+    {
+        $userName = $data['userName'];
+        $password = $data['password'];
+
+        if ($this->authService->login($userName, $password)) {
+            header("Location: /home");
+            exit;
+        } else {
+            $this->render('login', ['error' => 'Email ou senha inválida']);
+        }
+    }
+
+    public function createAccount(array $data): void
+    {
+        $userName = $data['userName'];
+        $password = $data['password'];
+        $confirmPassword = $data['confirmPassword'];
+
+        if ($password != $confirmPassword) {
+            $this->render('createAccount', ['error' => 'As senhas são diferentes']);
+            return;
+        }
+
+        if ($this->authService->createAccount($userName, $password)) {
+            header("Location: /home");
+            exit;
+        } else {
+            $this->render('login', ['error' => 'Username não disponível']);
+        }
+    }
+
+    public function logout() {
+        header("Location: /login");
+        exit;
+    }
+
+    public function render(string $view, array $data = []): void
+    {
+        extract($data);
+
+        ob_start();
+        require __DIR__ . "/../view/{$view}.php";
+        $content = ob_get_clean();
+
+        require __DIR__ . '/../view/layout.php';
+    }
 }
-
-?>
---
+++ app/service/AuthService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Service;
+
+use App\Controller\Main;
+use App\Model\User;
+
+class AuthService
+{
+    public function createAccount(string $userName, string $password): bool
+    {
+        $userName = Main::validateParameter($userName);
+        $password = Main::validateParameter($password);
+
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        $user = new User($userName, $hashedPassword);
+
+        if ($user->selectByUsername()) {
+            return false;
+        }
+        $user->insert();
+
+        return true;
+    }
+
+    public function login(string $userName, string $password): bool
+    {
+        $userName = Main::validateParameter($userName);
+        $password = Main::validateParameter($password);
+
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        $user = new User($userName, $hashedPassword);
+
+        $selectByUsername = $user->selectByUsername();
+        if (!$selectByUsername) {
+            return false;
+        }
+
+        foreach($selectByUsername[0] as $column => $value) {
+            $$column = $value;
+            \Connection\Log::addLog($column . ': ' . $value);
+        }
+
+        if ($user && password_verify($password, $userPassword)) {
+            $_SESSION['user_id'] = $userName;
+            return true;
+        }
+        return false;
+    }
+
+    public function logout(): void
+    {
+        unset($_SESSION['user_id']);
+        session_destroy();
+    }
+}
